0d3e8e23e7d515461ecb7409b0995e3cfb50886d
kimathinjoki <dfkimathinjoki@gmail.com>
2023-04-12 11:30:54 -0700

    completed

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,19 @@
+// function changeState(state, action) {
+//   switch (action.type) {
+//     case "counter/increment":
+//       return { count: state.count + 1 };
+//     default:
+//       return state;
+//   }
+// }
+
+// let state = { count: 0 };
+// let action = { type: "counter/increment" };
+
+// changeState(state, action);
+
+let state = { count: 0 };
+
 function changeState(state, action) {
   switch (action.type) {
     case "counter/increment":
@@ -7,7 +23,15 @@ function changeState(state, action) {
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function render() {
+  document.body.textContent = state.count;
+}
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+render(); 
+dispatch({type: "counter/increment"})
+dispatch({type: "counter/increment"})
